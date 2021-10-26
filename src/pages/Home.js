f144eebcf21da079c2893cbbf93c357c0b949dcf
Leon Michalak <leonmichalak6@gmail.com>
2021-10-26 22:34:12 +0100

    Setup home left intro card + layout for home right navbar (desktop view)

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,15 @@ import '../styles/home.css';
 export default function Home({ feedbackData }) {
   return (
     <main className='home'>
-      <h1>Default home page</h1>
+      <section className='home-left'>
+        <div className='home-left-intro'>
+          <h2 className='home-left-intro-title'>Frontend Mentor</h2>
+          <p className='body-s home-left-intro-subtitle'>Feedback board</p>
+        </div>
+      </section>
+      <section className='home-right'>
+        <nav className='home-right-nav'></nav>
+      </section>
     </main>
   );
 }
