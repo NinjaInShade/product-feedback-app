1c17161a45d0980bdd42769207e130ea6593918f
Leon Michalak <leonmichalak6@gmail.com>
2022-03-24 18:50:37 +0000

    added onclick prop to tab btn and function in tab filter layout component ready to update the current active tab

--- src/components/Tab.js
+++ src/components/Tab.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import '../styles/tab.css';
 
-export default function Tab({ title, active }) {
+export default function Tab({ title, active, onClick }) {
   return active !== undefined ? (
-    <button className={`tab tab-btn body-xs ${active ? 'active' : ''}`}>{title}</button>
+    <button className={`tab tab-btn body-xs ${active ? 'active' : ''}`} onClick={onClick}>
+      {title}
+    </button>
   ) : (
     <div className='tab body-xs'>{title}</div>
   );
--- src/components/layout/TabFilter.js
+++ src/components/layout/TabFilter.js
@@ -3,12 +3,16 @@ import Tab from '../Tab.js';
 import '../../styles/tab-filter.css';
 
 export default function TabFilter({ filterTabs, setFilterTabs }) {
+  const updateActiveTab = () => {
+    console.log('Update active tab');
+  };
+
   return filterTabs ? (
     <ul className='tab-filter'>
       {filterTabs.map((filterTab) => {
         return (
           <li key={filterTab.name}>
-            <Tab title={filterTab.name} active={filterTab.active} />
+            <Tab title={filterTab.name} active={filterTab.active} onClick={updateActiveTab} />
           </li>
         );
       })}
