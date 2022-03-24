45c4eb7c5c219a300a98c80b4a1af51adffb4626
Leon Michalak <leonmichalak6@gmail.com>
2022-03-24 18:58:37 +0000

    Can change current tab filter - suggestions aren't yet updated

--- src/components/layout/TabFilter.js
+++ src/components/layout/TabFilter.js
@@ -3,8 +3,16 @@ import Tab from '../Tab.js';
 import '../../styles/tab-filter.css';
 
 export default function TabFilter({ filterTabs, setFilterTabs }) {
-  const updateActiveTab = () => {
-    console.log('Update active tab');
+  const updateActiveTab = (name) => {
+    setFilterTabs(
+      filterTabs.map((filterTab) => {
+        if (filterTab.name === name) {
+          return { name, active: true };
+        }
+
+        return { ...filterTab, active: false };
+      })
+    );
   };
 
   return filterTabs ? (
@@ -12,7 +20,11 @@ export default function TabFilter({ filterTabs, setFilterTabs }) {
       {filterTabs.map((filterTab) => {
         return (
           <li key={filterTab.name}>
-            <Tab title={filterTab.name} active={filterTab.active} onClick={updateActiveTab} />
+            <Tab
+              title={filterTab.name}
+              active={filterTab.active}
+              onClick={() => updateActiveTab(filterTab.name)}
+            />
           </li>
         );
       })}
