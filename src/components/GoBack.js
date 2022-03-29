f69ed775c5cf8281e25d3b5a37b4aa2ac3b2e207
Leon Michalak <leonmichalak6@gmail.com>
2022-03-29 15:27:24 +0100

    Go back component styling done

--- src/components/GoBack.js
+++ src/components/GoBack.js
@@ -1,6 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../styles/go-back.css';
 
 export default function GoBack({ colour }) {
-  return <div>GoBack</div>;
+  return (
+    <Link to='/' className={`go-back ${colour === 'white' ? 'white' : ''}`}>
+      <svg width='6' height='10' viewBox='0 0 6 10' fill='none' xmlns='http://www.w3.org/2000/svg'>
+        <path d='M4.33447 9L0.334473 5L4.33447 1' stroke='#4661E6' stroke-width='2' />
+      </svg>
+      <span>Go Back</span>
+    </Link>
+  );
 }
