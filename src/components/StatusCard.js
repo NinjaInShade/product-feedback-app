743343d8bc03c19b9bfd05f96bdc538b6a869cea
Leon Michalak <leonmichalak6@gmail.com>
2022-03-30 15:30:50 +0100

    Status card componenet finished - need custom getProductRequets hook for updating data

--- src/components/StatusCard.js
+++ src/components/StatusCard.js
@@ -1,13 +1,35 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import StatusIndicator from './StatusIndicator';
+import UpvoteBtn from './UpvoteBtn.js';
+import CommentsCount from './CommentsCount';
+import Tab from './Tab.js';
 import '../styles/status-card.css';
 
 export default function StatusCard({ data }) {
+  const updateData = () => {
+    return;
+  };
+
   return (
     <div className={`status-card ${data.status}`}>
       {data.status === 'planned' && <StatusIndicator title='Planned' />}
       {data.status === 'in-progress' && <StatusIndicator title='In-Progress' />}
       {data.status === 'live' && <StatusIndicator title='Live' />}
+      <Link className='main' to={`/feedback/detail/${data.id}`}>
+        <h3>{data.title}</h3>
+        <p className='body'>{data.description}</p>
+        <Tab title={data.category} />
+      </Link>
+      <div className='comments-container'>
+        <UpvoteBtn
+          className='mobile'
+          suggestion={data}
+          updateSuggestion={updateData}
+          direction='horizontal'
+        />
+        <CommentsCount count={data.comments.length} />
+      </div>
     </div>
   );
 }
