50ad07fc4dc6e24bb80ae08e4228b41d2c89c0dd
Leon Michalak <leonmichalak6@gmail.com>
2022-04-01 21:55:06 +0100

    Feedback detail page is loading current feedback

--- src/pages/FeedbackDetail.js
+++ src/pages/FeedbackDetail.js
@@ -1,20 +1,26 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { Link, useParams } from 'react-router-dom';
 import GoBack from '../components/GoBack';
+import SuggestionCard from '../components/SuggestionCard';
+import { ProdReqContext } from '../context/ProdReqContext';
 import '../styles/feedback-detail.css';
 
 export default function FeedbackDetail() {
-  const ID = 2;
+  const [prodReqs] = useContext(ProdReqContext);
+  const { feedbackID } = useParams();
+
+  const currentProdReq = prodReqs.find((prodReq) => prodReq.id === parseInt(feedbackID));
 
   return (
     <div className='feedback-detail'>
       <div className='feedback-detail-container'>
         <div className='header'>
           <GoBack />
-          <Link to={`/feedback/edit/${ID}`} className='btn btn-secondary'>
+          <Link to={`/feedback/edit/${feedbackID}`} className='btn btn-secondary'>
             + Add Feedback
           </Link>
         </div>
+        <SuggestionCard suggestion={currentProdReq} />
       </div>
     </div>
   );
