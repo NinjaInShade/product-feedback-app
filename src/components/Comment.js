5d3144f5ae8bc529e6d52fc03a57f20f7f9b0c9f
Leon Michalak <leonmichalak6@gmail.com>
2022-04-03 13:35:19 +0100

    Single comments are working (where no replies are present), however reply button isnt implemented yet

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,22 +1,26 @@
 import React from 'react';
 import '../styles/comment.css';
 
-export default function Comment({ comment }) {
+export default function Comment({ comment, setComment }) {
   console.log(comment);
 
   return (
-    <div className={`comment ${comment.replies ? 'has-replies' : ''}`}>
-      <div className='avatar-container'>
-        <img
-          className='avatar'
-          src={`${comment.user.image.split('.')[1]}.jpg`}
-          alt={`Photograph of ${comment.user.name}`}
-        />
-        {comment.replies && <div className='replies-line'></div>}
-      </div>
+    <div className='comment'>
+      <img
+        className='avatar'
+        src={`${comment.user.image.split('.')[1]}.jpg`}
+        alt={`Photograph of ${comment.user.name}`}
+      />
       <div className='comment-main'>
-        <div className='comment-inital'></div>
-        {comment.replies && <ul className='comment-replies'></ul>}
+        <div className='comment-top'>
+          <div>
+            <h4 className='name'>{comment.user.name}</h4>
+            <small className='username'>@{comment.user.username}</small>
+          </div>
+          <button className='reply-btn body-xs'>Reply</button>
+        </div>
+
+        <p className='body-s comment-description'>{comment.content}</p>
       </div>
     </div>
   );
