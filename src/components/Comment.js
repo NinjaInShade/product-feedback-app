33fb711b2535a0493a09c05c9eb608cbea1b81fa
Leon Michalak <leonmichalak6@gmail.com>
2022-04-03 13:22:15 +0100

    Comment avatars being rendered in

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -2,5 +2,22 @@ import React from 'react';
 import '../styles/comment.css';
 
 export default function Comment({ comment }) {
-  return <div>Comment</div>;
+  console.log(comment);
+
+  return (
+    <div className={`comment ${comment.replies ? 'has-replies' : ''}`}>
+      <div className='avatar-container'>
+        <img
+          className='avatar'
+          src={`${comment.user.image.split('.')[1]}.jpg`}
+          alt={`Photograph of ${comment.user.name}`}
+        />
+        {comment.replies && <div className='replies-line'></div>}
+      </div>
+      <div className='comment-main'>
+        <div className='comment-inital'></div>
+        {comment.replies && <ul className='comment-replies'></ul>}
+      </div>
+    </div>
+  );
 }
