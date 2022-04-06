a2b72daf87070bbf243ffe8ffd0f2dfa8f346930
Leon Michalak <leonmichalak6@gmail.com>
2022-04-06 14:14:48 +0100

    Feedback form takes in functions as props for things such as delete & add/save changes. Also made parts responsive already

--- src/components/layout/FeedbackForm.js
+++ src/components/layout/FeedbackForm.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import IconNewFeedback from '../../assets/shared/icon-new-feedback.svg';
 import IconEditFeedback from '../../assets/shared/icon-edit-feedback.svg';
 import '../../styles/feedback-form.css';
 
-export default function FeedbackForm({ edit, feedback }) {
+export default function FeedbackForm({ edit, feedback, onDelete, onSubmit }) {
+  let history = useHistory();
+
+  const navigateBack = (e) => {
+    e.preventDefault();
+
+    history.goBack();
+  };
+
   return (
     <form className={`feedback-form ${edit ? 'edit' : ''}`}>
       {edit ? (
@@ -13,17 +22,25 @@ export default function FeedbackForm({ edit, feedback }) {
       )}
 
       {edit ? (
-        <h1 className='title'>Editing 'PUT FEEDBACK.TITLE'</h1>
+        <h1 className='title'>Editing '{feedback.title}'</h1>
       ) : (
         <h1 className='title'>Create New Feedback</h1>
       )}
       {/* INPUTS */}
 
       <div className='buttons'>
-        {edit && <button className='btn btn-danger'>Delete</button>}
+        {edit && (
+          <button className='btn btn-danger' onClick={(e) => onDelete(e)}>
+            Delete
+          </button>
+        )}
         <div>
-          <button className='btn btn-tertiary'>Cancel</button>
-          <button className='btn btn-primary'>{edit ? 'Save Changes' : 'Add Feedback'}</button>
+          <button className='btn btn-tertiary' onClick={(e) => navigateBack(e)}>
+            Cancel
+          </button>
+          <button className='btn btn-primary' onClick={(e) => onSubmit(e)}>
+            {edit ? 'Save Changes' : 'Add Feedback'}
+          </button>
         </div>
       </div>
     </form>
--- src/pages/FeedbackEdit.js
+++ src/pages/FeedbackEdit.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useParams } from 'react-router-dom';
 import GoBack from '../components/GoBack';
 import FeedbackForm from '../components/layout/FeedbackForm';
+import { ProdReqContext } from '../context/ProdReqContext';
 import '../styles/feedback-add.css';
 
 export default function FeedbackEdit() {
+  const [prodReqs] = useContext(ProdReqContext);
+
+  const { feedbackID } = useParams();
+
+  const currentFeedback = prodReqs.find((prodReq) => prodReq.id === parseInt(feedbackID));
+
   return (
     <div className='feedback-form-outer'>
       <div className='feedback-form-container'>
         <GoBack />
-        <FeedbackForm edit={true} />
+        <FeedbackForm edit={true} feedback={currentFeedback} />
       </div>
     </div>
   );
