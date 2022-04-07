9f75f34946db482d1cc368325a4559f3e3b44766
Leon Michalak <leonmichalak6@gmail.com>
2022-04-07 11:14:21 +0100

    input modifyied to allow text input AND textarea + error state

--- src/components/Input.js
+++ src/components/Input.js
@@ -3,15 +3,31 @@ import '../styles/input.css';
 
 export default function Input({ value, setValue, placeholderText, maxLength, ID, type, error }) {
   return (
-    <textarea
-      type='text'
-      className='body-s input'
-      maxLength={maxLength}
-      placeholder={placeholderText}
-      value={value}
-      onChange={(e) => setValue(e.target.value)}
-      name={ID}
-      id={ID}
-    />
+    <>
+      {type === 'text' && (
+        <input
+          type='text'
+          className={`body-s input text ${error ? 'error' : ''}`}
+          maxLength={maxLength}
+          placeholder={placeholderText}
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          name={ID}
+          id={ID}
+        />
+      )}
+      {type === 'textarea' && (
+        <textarea
+          type='text'
+          className={`body-s input ${error ? 'error' : ''}`}
+          maxLength={maxLength}
+          placeholder={placeholderText}
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          name={ID}
+          id={ID}
+        />
+      )}
+    </>
   );
 }
