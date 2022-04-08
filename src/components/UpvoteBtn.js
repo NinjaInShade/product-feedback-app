a8d86f8de9a02eae485f11975852b838e1877091
Leon Michalak <leonmichalak6@gmail.com>
2022-04-08 12:29:01 +0100

    Enhanced upvote btn to fix number not changing when upvoting

--- src/components/UpvoteBtn.js
+++ src/components/UpvoteBtn.js
@@ -34,7 +34,7 @@ export default function UpvoteBtn({ suggestion, className, direction }) {
       <svg width='10' height='7' xmlns='http://www.w3.org/2000/svg'>
         <path d='M1 6l4-4 4 4' stroke='#4661E6' strokeWidth='2' fill='none' fillRule='evenodd' />
       </svg>
-      <b className='body-xs'>{suggestion.upvotes}</b>
+      <b className='body-xs'>{active ? suggestion.upvotes + 1 : suggestion.upvotes}</b>
     </button>
   );
 }
